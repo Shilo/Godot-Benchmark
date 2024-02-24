137b6c1a5cbde411054a57eb7ceb3608302af3a5
Shilo <shilo86@gmail.com>
2024-02-24 07:29:40 -0800

    reordered ui rendering

--- Benchmarks/Nodes/2DSprites/GDS/2DSpritesNodes.gd
+++ Benchmarks/Nodes/2DSprites/GDS/2DSpritesNodes.gd
@@ -1,11 +1,9 @@
 extends Node
 
-@export var sprite_texture: Texture2D
-
-@onready var scene = get_parent()
+var sprite_texture: Texture2D = load("res://icon.svg")
 
 func _ready():
-	scene.spawn.connect(spawn)
+	get_node("%Benchmark").spawn.connect(spawn)
 
 func spawn():
 	var sprite := Sprite2D.new()
@@ -15,4 +13,4 @@ func spawn():
 	var half_size := sprite.texture.get_size() / 2
 	sprite.position.x = randf_range(half_size.x, viewport_size.x - half_size.x)
 	sprite.position.y = randf_range(half_size.y, viewport_size.y - half_size.y)
-	scene.add_child(sprite)
+	add_child(sprite)
