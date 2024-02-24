323d8f1c677a07af2636db27968ab6d7415d7c57
Shilo <shilo86@gmail.com>
2024-02-24 03:49:45 -0800

    benchmark scene

--- BaseBenchmark/BaseBenchmark.gd
+++ BaseBenchmark/BaseBenchmark.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+const ignore_chars_drawn: int = 13
+
 @export var font_size: int = 16
 @export var font: Font
 
 var context: RID
+var last_chars_drawn: int
 
 func _ready():
 	if !font: font = ThemeDB.fallback_font
@@ -15,11 +18,21 @@ func _ready():
 func _process(delta: float):
 	RenderingServer.canvas_item_clear(context)
 	
-	var text: String = "%s\n\nFRAMES/SEC: %s\nDRAWN OBJS: %s\nDRAW CALLS: %s" % [
+	var drawn_objs: int = Performance.get_monitor(Performance.RENDER_TOTAL_OBJECTS_IN_FRAME) - last_chars_drawn
+	var draw_calls: int = Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME) - last_chars_drawn
+	
+	var text: String = "%s\n(PRESS TO GO BACK)\n\nFRAMES/SEC: %s\nDRAWN OBJS: %s\nDRAW CALLS: %s" % [
 		name.to_upper(),
 		Engine.get_frames_per_second(),
-		Performance.get_monitor(Performance.RENDER_TOTAL_OBJECTS_IN_FRAME),
-		Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME)
+		drawn_objs,
+		draw_calls
 	]
 	
 	font.draw_multiline_string(context, Vector2.ZERO, text, 0, -1, font_size)
+	
+	last_chars_drawn = text.length() - ignore_chars_drawn
+
+func _input(event: InputEvent):
+	if event is not InputEventMouseButton || !event.is_pressed(): return
+	
+	get_tree().change_scene_to_file("res://Main/Main.tscn")
